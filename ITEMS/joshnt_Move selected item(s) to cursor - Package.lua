33e7a159d227b76654049a8d54c7504e593e6675
Joshn't <joshua-hank@web.de>
2024-08-22 19:12:12 +0200

    V1.1 move sel items to mouse

--- ITEMS/joshnt_Move selected item(s) to cursor - Package.lua
+++ ITEMS/joshnt_Move selected item(s) to cursor - Package.lua
@@ -1,12 +1,14 @@
 -- @description Move sel. items to cursor
--- @version 1.0
+-- @version 1.1
 -- @author Joshnt
 -- @about
 --      Moves items to the selected track - offers other actions to keep relative position between selected items
 -- @changelog
---  + init - Thanks to McWolverine for the idea!
+--  - added "... - only time" and "... - only track" variants
 -- @metapackage
 -- @provides
 --   [main] joshnt_Move selected item(s) to cursor/joshnt_Move selected item(s) to mouse cursor.lua
 --   [main] joshnt_Move selected item(s) to cursor/joshnt_Move selected item(s) to mouse cursor - keep relative position (time).lua
 --   [main] joshnt_Move selected item(s) to cursor/joshnt_Move selected item(s) to mouse cursor - keep relative position (time+tracks).lua
+--   [main] joshnt_Move selected item(s) to cursor/joshnt_Move selected item(s) to track under mouse cursor - keep position.lua
+--   [main] joshnt_Move selected item(s) to cursor/joshnt_Move selected item(s) to position under mouse cursor - keep track.lua
