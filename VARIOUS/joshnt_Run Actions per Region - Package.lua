e770b727a3fd55bd6310e0af27cfa8c18eaf7040
Joshn't <joshua-hank@web.de>
2025-05-10 23:31:20 +0200

    renamed to batch

--- VARIOUS/joshnt_Run Actions per Region - Package.lua
+++ VARIOUS/joshnt_Run Actions per Region - Package.lua
@@ -1,15 +1,15 @@
 -- @description Run Action over over items/tracks/time on region (individually per region)
--- @version 1.03
+-- @version 1.04
 -- @author Joshnt
 -- @about
 --    Run Action over items/tracks/time on region (individually per region) - script selects items, tracks, time and envelope points per region
 --    Usecase e.g. SWS_Set selected items to One Random Custom Color, joshnt_ReaGlue-Regions.lua, ...
 -- @changelog
---  + init
+--  - added batch normalization (name adjust)
 -- @metapackage
 -- @provides
 --   [main] joshnt_Run Actions for items tracks time or envelope of region (individually per region)/joshnt_Run Action for items tracks or time in all regions (individually per region).lua
 --   [main] joshnt_Run Actions for items tracks time or envelope of region (individually per region)/joshnt_Run Action for items tracks or time in regions in time selection (individually per region).lua
 --   [main] joshnt_Run Actions for items tracks time or envelope of region (individually per region)/joshnt_Run Action for items tracks or time in selected regions (individually per region).lua
 --   [main] joshnt_Run Actions for items tracks time or envelope of region (individually per region)/joshnt_Run Action for selected items (individually per overlapping item stack).lua
---   [main] joshnt_Run Actions for items tracks time or envelope of region (individually per region)/joshnt_Normalize selected items - TruePeak -6dB loudest item (individually per overlapping item stack).lua
+--   [main] joshnt_Run Actions for items tracks time or envelope of region (individually per region)/joshnt_Batch Normalize selected items - TruePeak -6dB loudest item (individually per overlapping item stack).lua
